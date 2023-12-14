327e5000d3165ac087643f986667e233c39bc8c7
Faisal Tareque <faisaltareque98@gmail.com>
2023-12-14 09:11:07 -0500

    Updated Setup, README and Example Notebook

---
+++ BanglaLanguageToolkit/__init__.py
@@ -0,0 +1,1 @@
+from .bangla_text_cleaner import BanglaTextCleaner
--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    description = fh.read()
+
 setup(
     name="BanglaLanguageToolkit",
-    version="0.1",
+    version="0.2",
     packages=find_packages(include=['BanglaLanguageToolkit', 'BanglaLanguageToolkit.*']),
     install_requires=[
         "ftfy==6.1.1",
         "emoji==1.4.2",
     ],
+    long_description=description,
+    long_description_content_type="text/markdown",
 )
